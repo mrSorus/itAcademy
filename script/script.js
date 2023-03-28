6bed85a6c6fc25acb9d657c5268bd5c5273abdd3
sergeyZhevnov <mr.sorus@gmail.com>
2023-03-28 19:53:57 +0300

    домашняя работа №1

--- script/script.js
+++ script/script.js
@@ -1,1 +1,14 @@
-console.log(`hello word`);
+// console.log(`hello word`);
+
+function outer() {
+  let count = 0;
+
+  function inner() {
+    count++;
+    console.log(count);
+  }
+
+  return inner;
+}
+
+const increment = outer();
