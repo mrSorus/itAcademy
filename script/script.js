c0a834c7b62e8de5b92a3a015958aefd784a5a12
sergeyZhevnov <mr.sorus@gmail.com>
2023-04-08 10:32:15 +0300

    разбор дз

--- script/script.js
+++ script/script.js
@@ -1,58 +1,31 @@
-// 1. создать пустую форму
-// 2. перебрать массив данных, создать массив для новых элементов
-// 3. на каждой итерации создать элемент формы
-// - поле type - определяет тип документа
-// - id - id и name 
-// - value - его текущее значение
-// - options - доступные опции (несколько элементов)
-// 4. создаем кнопку с обработчиками
-// 5. добавляем элементы в форму
+const array = [
+{
+	type: 'radio',
+	id: 'age',
+	value: 1,
+	options: [1,2,3,4]
+}
+]
 
+const form = document.querySelector('#form');
 
-// const { log } = require("console")
+array.forEach(el => {
+	if (Array.isArray(el.options)){
+		el.options.forEach(options => {
+			const label = document.createElement('label');
+			const title = document.createElement('span')
+			const chekboxElement = document.createElement('input')
 
-// const input = document.querySelector("#text");
-// const checkbox = document.querySelector("#checkbox");
+			chekboxElement.type = 'radio';
+			chekboxElement.id = options;
+			chekboxElement.name = el.id;
+			chekboxElement.value = options;
 
-// document.addEventListener('change', (e) => {
-//   console.log(e.target.value);
-// })
+			title.innerText = option;
 
-// const elements = document.querySelectorAll('body *');
-
-// elements.forEach(element => {
-// element.addEventListener('click', (e) => {
-//   console.log(e.currentTarget);
-// })
-// });
-
-// const root = document.querySelector('#parent');
-// const body = document.body;
-
-// root.addEventListener('click', (e) => {
-//   console.log(e)
-// })
-
-// const customClick = new Event('click', {
-//   clientX: 100,
-//   clientY: 200
-// })
-
-// body.addEventListener('click', (e) => {
-//   console.log(e);
-//   root.dispatchEvent(customClick);
-// })
-
-
-
-// root.dispatchEvent(customClick);
-
-// body.addEventListener("click", (event) => {
-//   const x = event.clientX;
-//   const y = event.clientY;
-//   const heigt = event.target.offsetHeight;
-//   const wight = event.target.offsetWidth;
-//   console.log(wight, heigt);
-//   console.log("Координаты клика мышью: " + x + ", " + y);
-// });
-
+			label.append(title);
+			form.append(chekboxElement);
+			form.append(label)
+		})
+	}
+})
